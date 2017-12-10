27cff314bdd10e0a0fe8ca9bada7abbe3ca1a64f
Dustin Autery <dustin.autery@taillight.com>
2017-12-09 23:16:59 -0600

    register an empty nagivation event handler so the debugger quites whining

--- tv/src/App.js
+++ tv/src/App.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import {
   View,
   StyleSheet,
-  FlatList,
-  Text,
-  Image,
-  TouchableOpacity,
 } from 'react-native';
 import { Provider } from 'react-redux';
 import { store } from './redux';
@@ -13,10 +9,29 @@ import * as WebSocket from './websocket';
 
 import { AppNavigator } from './modules/app';
 
+const TVEventHandler = require('TVEventHandler'); // eslint-disable-line
+
 class App extends Component {
 
   componentDidMount() {
     WebSocket.init();
+    this._enableTVEventHandler();
+  }
+
+  componentWillUnmount() {
+    this._disableTVEventHandler();
+  }
+
+  _enableTVEventHandler() {
+    this._tvEventHandler = new TVEventHandler();
+    this._tvEventHandler.enable(this, (cmp, evt) => {});
+  }
+
+  _disableTVEventHandler() {
+    if (this._tvEventHandler) {
+      this._tvEventHandler.disable();
+      delete this._tvEventHandler;
+    }
   }
 
   render() {
