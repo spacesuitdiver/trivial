8302ff2eef9f1654a4009423ee74f7416bed76ed
Dustin Autery <dustin.autery@taillight.com>
2017-12-10 22:37:25 -0600

    cleanup welcome screen

--- tv/src/modules/round/components/welcome/index.js
+++ tv/src/modules/round/components/welcome/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import {
     View,
     Text,
-    TouchableHighlight,
     StyleSheet,
 } from 'react-native';
-import AnimatedLinearGradient, { presetColors } from 'react-native-animated-linear-gradient';
-import LinearGradient from 'react-native-linear-gradient';
 
 import Screen from '../Screen';
+import Button from '../Button';
 
 import { send } from '../../../../websocket';
 
@@ -16,28 +14,27 @@ const colors = {
   black: 'black',
   orange: 'orange',
   white: 'white',
+  transparent: 'transparent',
+  grey: '#222222',
 };
 
 const styles = StyleSheet.create({
   title: {
-    backgroundColor: 'transparent',
+    backgroundColor: colors.transparent,
     marginBottom: 60,
     flex: 0,
     flexDirection: 'row',
-    padding: 40
+    padding: 40,
   },
   titleText: {
     fontSize: 144,
-    color: '#222222'
-  },
-  button: {
-    backgroundColor: 'transparent',
-    padding: 30,
+    color: colors.grey,
   },
   text: {
     color: colors.white,
     fontSize: 72,
   },
+  italic: { fontStyle: 'italic' },
 });
 
 class Welcome extends React.Component {
@@ -52,32 +49,26 @@ class Welcome extends React.Component {
     }), 1000);
   }
 
+  begin = () => {
+    send({
+      resource: 'round',
+      action: 'nextQuestion',
+    });
+    this.props.navigation.navigate('Question');
+  };
+
   render() {
     return (
       <Screen>
         <View style={styles.title}>
           <Text style={styles.titleText}>Welcome to</Text>
-          <Text style={[styles.titleText, { fontStyle: 'italic' }]}>TRIVIAL</Text>
+          <Text style={[styles.titleText, styles.italic]}>TRIVIAL</Text>
         </View>
-        <View>
-          <TouchableHighlight
-            onPress={() => {
-              send({
-                resource: 'round',
-                action: 'nextQuestion',
-              });
-              this.props.navigation.navigate('Question');
-            }}
-            style={{ borderRadius: 30, overflow: 'hidden' }}
-          >
-            <LinearGradient
-              colors={['#222222', '#2b2b2b', '#2f2f2f']}
-              style={styles.button}
-            >
-              <Text style={styles.text}>START NEW GAME!</Text>
-            </LinearGradient>
-          </TouchableHighlight>
-        </View>
+        <Button
+          onPress={this.begin}
+        >
+          <Text style={styles.text}>Let&rsquo;s Play!</Text>
+        </Button>
       </Screen>
     );
   }
