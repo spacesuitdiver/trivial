644c5e86acd2c6c832f5a9f597b8b3c4e9ca2e77
Dustin Autery <dustin.autery@taillight.com>
2017-12-10 22:37:40 -0600

    add buttons to queestions screen

--- tv/src/modules/round/components/question/index.js
+++ tv/src/modules/round/components/question/index.js
@@ -2,35 +2,79 @@ import React from 'react';
 import {
     View,
     Text,
-    TouchableHighlight,
     StyleSheet,
 } from 'react-native';
 import { connect } from 'react-redux';
 
 import Screen from '../Screen';
 import Players from '../Players';
+import Button from '../Button';
+
+import { send } from '../../../../websocket';
+
+const nextQuestion = () => send({
+  resource: 'round',
+  action: 'nextQuestion',
+});
+
+const QuestionScreen = ({ question, navigation }) => (
+  <Screen>
+    <View style={styles.upper}>
+      <View style={styles.question}>
+        { question && <Text style={styles.questionText}>{ question.text }</Text> }
+      </View>
+      <View style={styles.buttonRow}>
+        <Button
+          style={{ marginRight: 20 }}
+          onPress={nextQuestion}
+        >
+          <Text style={styles.text}>NEXT</Text>
+        </Button>
+        <Button
+          style={{ marginRight: 20 }}
+          onPress={() => navigation.navigate('Winner')}
+        >
+          <Text style={styles.text}>FINISH</Text>
+        </Button>
+      </View>
+    </View>
+    <Players />
+  </Screen>
+);
+
+const colors = {
+  opaque: 'rgba(255,255,255,0.25)',
+  white: 'white',
+};
 
 const styles = StyleSheet.create({
-  question: {
-    width: '100%',
+  upper: {
     paddingTop: 50,
     paddingBottom: 50,
-    backgroundColor: 'rgba(255,255,255,0.25)',
+    backgroundColor: colors.opaque,
+    flexDirection: 'column',
+    width: '100%',
+    flex: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  question: {
+    width: '80%',
   },
   questionText: {
     fontSize: 72,
     textAlign: 'center',
   },
+  text: {
+    color: colors.white,
+    fontSize: 36,
+  },
+  buttonRow: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 30,
+  },
 });
-
-const QuestionScreen = ({ question }) => (
-  <Screen>
-    <View style={styles.question}>
-      { question && <Text style={styles.questionText}>{ question.text }</Text> }
-    </View>
-    <Players />
-  </Screen>
-);
 
 const mapStateToProps = state => ({
   question: state.round.default.question,
